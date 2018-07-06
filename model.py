e2aa7562b39093c9be87036eb9a2ff68554ce833
Roy Veshovda <roy@veshovda.no>
2018-07-07 00:44:08 +0200

    feat: Implemented

--- model.py
+++ model.py
@@ -23,11 +23,29 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        embeddings = self.embed(captions[:,:-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _  = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        sampled_ids = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            _, predicted = outputs.max(1)
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        sampled_ids = torch.stack(sampled_ids, 1)
+        sampled_ids = sampled_ids.view((-1))
+        
+        return sampled_ids.cpu().tolist()
